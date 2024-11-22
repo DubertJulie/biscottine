751157c8ca8a06ef6558c6b4cb98de49c963984a
Emerick Roeting <decdu59@live.fr>
2024-11-22 23:30:53 +0100

    feat/Ajout Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import IngredientInput from './components/IngredientInput';
+import Header from './components/Header';
+import IngredientInput from './components/IngredientInput'; 
 
 
 function App() {
   return (
     <div>
-
+      <Header />
       <IngredientInput />
-
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/IngredientInput.js
+++ src/components/IngredientInput.js
@@ -1,24 +1,23 @@
-
 import './IngredientInput.css';
 import mascotteBiscottine from '../assets/mascotte.png'
-import LogoInputIngredient from '../assets/logo.png'
+import logoInputIngredient from '../assets/logo.png'
+
 
 
 function ingredientInput() {
-
 
     return (
 
         <div className="encadrementIngredientInput">
 
-            
-{/* Image de la mascotte  */}
-                <img className='biscottineMascotteInput' src={mascotteBiscottine} alt='' />
-               {/* Image logo  */}
-                <img className='titreLogoInput' src={LogoInputIngredient} alt='' />
+
+            {/* Image de la mascotte  */}
+            <img className='biscottineMascotteInput' src={mascotteBiscottine} alt='' />
+            {/* Image logo  */}
+            <img className='titreLogoInput' src={logoInputIngredient} alt='' />
             {/* Texte entredessous du Logo  */}
-                <p className='texteSousLogoInput'>Trouve-moi <br/> une recette !</p>
-           
+            <p className='texteSousLogoInput'>Trouve-moi <br /> une recette !</p>
+
             <p></p>
             <p></p>
             <input className="ingredientInput" type="text" placeHolder=" Yes" />
