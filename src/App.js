c0e6abb50b9af37daca5d62f07425115249a3b41
Julie <63109043+DubertJulie@users.noreply.github.com>
2024-11-23 17:34:58 +0100

    Revert "Ingredients input & header "

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import IngredientInput from './components/IngredientInput'; 
-
 
 function App() {
   return (
-    <div>
-      <Header />
-      <IngredientInput />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
